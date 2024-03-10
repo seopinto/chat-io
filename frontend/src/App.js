08c0b4597c9797e3978bd1e30e01502e253785e8
seopinto <s.andrespinto@gmail.com>
2024-03-09 20:41:25 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import ConnectedUsers from "./components/ConnectedUsers";
 const socket = io("http://localhost:4000");
 
 function App() {
-  const [alias, setAlias] = useState("");
+  const [alias, setAlias] = useState(() => localStorage.getItem('userAlias') || "");
   const [currentUserId, setCurrentUserId] = useState(null);
   const [users, setUsers] = useState([]);
   const [currentRoom, setCurrentRoom] = useState(null);
@@ -15,19 +15,28 @@ function App() {
   const [newMessagesFromUsers, setNewMessagesFromUsers] = useState({});
   const [selectedUserAlias, setSelectedUserAlias] = useState('');
 
-
   useEffect(() => {
-    socket.on("connect", () => console.log("Connected to server"));
+    const storedUserId = localStorage.getItem('userId');
+    if(storedUserId) {
+      setCurrentUserId(storedUserId);
+    }
+
+    socket.on("connect", () => {
+      console.log("Connected to server");
+      // Al conectarse, si ya tenemos un alias almacenado, reemitimos el alias para establecerlo en el servidor.
+      if(alias) {
+        socket.emit("setAlias", alias);
+      }
+    });
+
     socket.on("disconnect", () => console.log("Disconnected from server"));
 
     socket.on("usersList", (updatedUsers) => setUsers(updatedUsers));
+
     socket.on("newMessage", (message) => {
-      // Si el usuario actual está en la sala y el mensaje es para esta sala, agregar el mensaje al estado
       if (currentRoom && message.roomId === currentRoom) {
         setMessages((prevMessages) => [...prevMessages, message]);
       }
-    
-      // Si el mensaje es para el usuario actual y no lo ha enviado él, actualiza las notificaciones
       if (message.senderId !== currentUserId) {
         setNewMessagesFromUsers((prevNotifications) => ({
           ...prevNotifications,
@@ -38,6 +47,7 @@ function App() {
 
     socket.on("yourUserId", (userId) => {
       setCurrentUserId(userId);
+      localStorage.setItem('userId', userId); // Guarda el userId en localStorage
       console.log("UserID set to:", userId);
     });
 
@@ -48,9 +58,10 @@ function App() {
       socket.off("usersList");
       socket.off("yourUserId");
     };
-  }, [currentRoom, currentUserId]);
+  }, [currentRoom, currentUserId, alias]);
 
   const handleAliasSubmit = (userAlias) => {
+    localStorage.setItem('userAlias', userAlias); // Guarda el alias en localStorage
     setAlias(userAlias);
     socket.emit("setAlias", userAlias);
   };
@@ -59,42 +70,40 @@ function App() {
     const roomId = [currentUserId, selectedUser.id].sort().join("-");
     setCurrentRoom(roomId);
     setSelectedUserAlias(selectedUser.alias);
-
     setNewMessagesFromUsers((prevNotifications) => ({
-        ...prevNotifications,
-        [selectedUser.id]: false
+      ...prevNotifications,
+      [selectedUser.id]: false
     }));
-
-      socket.emit("joinRoom", { roomId, userId: currentUserId });
+    socket.emit("joinRoom", { roomId, userId: currentUserId });
     console.log("Joining room:", { roomId, userId: currentUserId });
     setMessages([]);
-};
+  };
 
   return (
     <div className="App bg-gray-800 dark:bg-gray-800 h-screen">
-    {!alias ? (
-      <UserAliasForm onAliasSubmit={handleAliasSubmit} />
-    ) : (
-      <div className="container mx-auto flex">
-        <ConnectedUsers
-          users={users}
-          onSelectUser={handleSelectUser}
-          currentUserId={currentUserId}
-          userAlias={alias}
-          newMessagesFromUsers={newMessagesFromUsers} // Pasar el estado de notificaciones
-        />
-        {currentRoom && (
-          <ChatContainer
-          roomId={currentRoom}
-          currentUserId={currentUserId}
-          messages={messages}
-          selectedUserAlias={selectedUserAlias}
-          socket={socket} // Pasa la instancia de socket
-        />
-        )}
-      </div>
-    )}
-  </div>
+      {!alias ? (
+        <UserAliasForm onAliasSubmit={handleAliasSubmit} />
+      ) : (
+        <div className="container mx-auto flex">
+          <ConnectedUsers
+            users={users}
+            onSelectUser={handleSelectUser}
+            currentUserId={currentUserId}
+            userAlias={alias}
+            newMessagesFromUsers={newMessagesFromUsers}
+          />
+          {currentRoom && (
+            <ChatContainer
+              roomId={currentRoom}
+              currentUserId={currentUserId}
+              messages={messages}
+              selectedUserAlias={selectedUserAlias}
+              socket={socket}
+            />
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
